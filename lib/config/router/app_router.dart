f4cbc8553ed3f955dd656275a896a641bdfe6163
luco-hub <nan2boom93@gmail.com>
2025-06-02 00:58:35 -0500

    estructura inicial

---
+++ lib/config/router/app_router.dart
@@ -0,0 +1,14 @@
+
+import 'package:cinemapedia/presentation/screens/screens.dart';
+import 'package:go_router/go_router.dart';
+
+final appRouter = GoRouter(
+  initialLocation: '/',
+  routes: [
+    GoRoute(
+      path: '/',
+      name: HomeScreen.nameRouter,
+      builder: (context, state) => const HomeScreen(),
+      )
+  ]
+);
---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,13 @@
+
+
+
+import 'package:flutter/material.dart';
+
+class AppTheme {
+
+
+  ThemeData getTheme() => ThemeData(
+      colorSchemeSeed: const Color(0xFF2862F5)
+  );
+
+}
